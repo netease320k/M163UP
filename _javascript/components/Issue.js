69b58cb39565fc2ae8db9bd805603f45f4fdb9bf
胡凡 <hufan.Akarin@Gmail.com>
2016-09-10 17:01:13 +0800

    change style

--- _javascript/components/Issue.js
+++ _javascript/components/Issue.js
@@ -10,8 +10,8 @@ const IssueHeader = ({title, labels})=>(
                 <Label
                     key={label.name}
                     className="label"
-                    color={label.color}
                     active={true}
+                    style={{ outlineColor: `#${label.color}`}}
                 >{label.name}</Label>
             )}
         </ul>
@@ -34,7 +34,7 @@ const Issue = ({issue,showDetail})=>(
             <a href={issue.user.html_url}>{issue.user.login}</a>
             <span> 提交于 {convertDateTime(issue.created_at)} </span>
             {issue.closed_at?<span>处理于 {convertDateTime(issue.closed_at)}</span>:issue.created_at==issue.updated_at?null :<span> 更新于 {convertDateTime(issue.updated_at)} </span>}
-            <a className="button" href={issue.html_url} target="_blank">查看详细</a>
+            <a className="button out-link" href={issue.html_url} target="_blank">查看详细</a>
         </IssueFooter>
     </section>
 );
